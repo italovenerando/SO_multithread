0f73c3c07920c7fbcc3730ece05ed6d80d8c2ffb
Augusto Toledo Caires de Oliveira <66071603+augustotcoliveira@users.noreply.github.com>
2024-11-12 21:18:13 -0300

    Update sort.c

--- src/sort.c
+++ src/sort.c
@@ -3,29 +3,7 @@
 #include <stdlib.h>
 #include "../include/sort.h"
 
-void quick(int *vet, int esq, int dir) { // Usada para organizar o vetor final
-    int pivo = esq, i, ch, j;
-    for (i = esq + 1; i <= dir; i++) {
-        j = i;
-        if (vet[j] < vet[pivo]) {
-            ch = vet[j];
-            while (j > pivo) {
-                vet[j] = vet[j - 1];
-                j--;
-            }
-            vet[j] = ch;
-            pivo++;
-        }
-    }
-    if (pivo - 1 >= esq) {
-        quick(vet, esq, pivo - 1);
-    }
-    if (pivo + 1 <= dir) {
-       quick(vet, pivo + 1, dir);
-    }
-}
-
-void mergeSort(int *vetor, int posicaoInicio, int posicaoFim) { // Usada para organizar os valores de cada arquivo usando as threads
+void mergeSort(int *vetor, int posicaoInicio, int posicaoFim) { // Usada para organizar os valores de cada arquivo usando as threads e depois organizar o vetor final
     int i, j, k, metadeTamanho, *vetorTemp;
     if (posicaoInicio == posicaoFim) return;
     metadeTamanho = (posicaoInicio + posicaoFim) / 2;
